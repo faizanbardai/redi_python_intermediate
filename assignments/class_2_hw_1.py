b6a8b1068b040bcf183fde0d8474a17701f0c4a1
FayJu <faizan@unetiq.com>
2022-03-23 21:08:15 +0100

    added comment

--- assignments/class_2_hw_1.py
+++ assignments/class_2_hw_1.py
@@ -1,5 +1,10 @@
+# Find the index of the first occurrence of a substring in a string.
+# So given the word “The worlds fastest plane”, find the substring “plane” and “car” and print
+# the output.
+
 string = 'The worlds fastest plane'
 to_test = ['plane', 'car']
 
 for i in to_test:
     print(i, string.find(i))
+    print(i in string)
