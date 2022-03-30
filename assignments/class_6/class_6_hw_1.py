f84fc469f0ba34a2db16bd3d907eeb2ca97bbe2a
FayJu <faizan@unetiq.com>
2022-03-30 21:18:29 +0200

    class 6 hw 1

---
+++ assignments/class_6/class_6_hw_1.py
@@ -0,0 +1,16 @@
+# In your code, create a Dictionary that contains 3 secret words as keys. Use anything as values.
+secret_words = {'foo': 123, 'hello': 456}
+
+# Ask the user to input the any word.
+user_word = input('Enter a word: ')
+
+# If the word works as a key for the dictionary, print the key and the value
+# If the word does NOT work as a key for the dictionary, catch the resulting exception and
+# inform the user about the error using a print or logging statement
+# Important: Don’t use if clauses. Try to access the dictionary by key and handle the error.
+
+
+try:
+    print(user_word, secret_words[user_word])
+except KeyError:
+    print('This word is not part of my secret dictionary :)')
