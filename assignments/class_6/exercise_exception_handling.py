692cefe51e8386c7c19ce15745a19fa27dc5b63e
FayJu <faizan@unetiq.com>
2022-03-30 20:23:03 +0200

    assignment done

--- assignments/class_6/exercise_exception_handling.py
+++ assignments/class_6/exercise_exception_handling.py
@@ -1,19 +1,22 @@
+def multiply_with_allergy(x, y):
+    if x == 42 or y == 42:
+        raise ValueError("Sorry, I'm allergic to 42")
+    else:
+        result = x*y
+        return result
 
-def multiply_with_allergy(x,y):
-	if x==42 or y==42:
-		raise ValueError("Sorry, I'm allergic to 42")
-	else:
-		result = x*y
-		return result
 
 def square(x):
-	return multiply_with_allergy(x,x)
+    try:
+        return multiply_with_allergy(x, x)
+    except ValueError:
+        return x*x
+
 
 number_as_str = input("Enter a number to square: ")
 number = int(number_as_str)
 
 # TODO: try to square (also in case of 42)
-result=square(number)
+result = square(number)
 # TODO: except if a ValueError occurs, then square "manually" using result=x*x
 print(f"The square is: {result}")
-
