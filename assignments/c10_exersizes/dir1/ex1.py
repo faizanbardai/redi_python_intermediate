ef43a8d22c7926c4abe2187a240f1b945cf2144a
FayJu <faizan@unetiq.com>
2022-04-15 14:43:32 +0200

    ex1

--- assignments/c10_exersizes/dir1/ex1.py
+++ assignments/c10_exersizes/dir1/ex1.py
@@ -8,7 +8,7 @@ def first_item_equals_last_item(l):
     a) Write a function that takes a list of integers and returns True if the first and last number are equal and False otherwise.
     Example: first_last_equal([1,2,3,2,1]) should return True
     """
-    pass
+    return l[0] == l[-1]
 
 
 def get_even_indexed_chars(s: str) -> str:
@@ -18,7 +18,7 @@ def get_even_indexed_chars(s: str) -> str:
     - picks only the characters at every even position (e.g. the 0th, 2nd, 4th, ... character)
     - returns the selected characters as a string
     """
-    pass
+    return s[::2]
 
 
 def remove_chars(s, n):
@@ -26,7 +26,7 @@ def remove_chars(s, n):
     c) Write a function that takes a string and a number n. It should remove n characters from a string starting from zero up to n and return the new string.
     Example: remove_chars("python", 2) should return "thon"
     """
-    pass
+    return s[n:]
 
 
 def one_in_common(s1, s2):
@@ -35,7 +35,7 @@ def one_in_common(s1, s2):
     returns True if they have at least one element in common
     and False otherwise.
     """
-    pass
+    return len(s1.intersection(s2)) > 0
 
 
 def is_palindrome_number(n):
@@ -43,5 +43,4 @@ def is_palindrome_number(n):
     e) Write a function that takes a number and returns True if it is a palindrome number,
     otherwise it returns False. A palindrome number is a number that is same after reverse.
     """
-
-    pass
+    return str(n) == str(n)[::-1]
