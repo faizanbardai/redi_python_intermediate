6e57d89cd4f3d33986756c2719067a0b4158a846
FayJu <faizan@unetiq.com>
2022-04-17 06:27:46 +0200

    ex 3

--- assignments/c10_exersizes/dir3/ex3.py
+++ assignments/c10_exersizes/dir3/ex3.py
@@ -8,26 +8,38 @@ a) Write a program that will do the following:
     4. Use your caesar_encrypt(original_text, step) function to decrypt your friends message and write the result in a new file, „decrypted_message.txt", using the file handling from previous lessons. Tipp: your friends favorite number is 5!
 
 """
-# TODO: 1. import the shift_letter function from shift_module.
 from .shift_module import shift_letter
+import pathlib
 
 
 def caesar_encrypt(original_text: str, step: int) -> str:
     """
-    TODO: 3. Implement the function "caesar_encrypt(original_text, step)"
+     Implement the function "caesar_encrypt(original_text, step)"
      that will take a string as input and return (not print!)
      the string with all letters shifted by [step] places.
     """
-    pass
+
+    return ''.join([shift_letter(x, step) for x in original_text])
 
 
 def decypher_file(input_file_path: str, output_file_path: str) -> None:
-    # TODO: 2. Open the secret_message.txt file
+    #  2. Open the secret_message.txt file
     #  and save all of its content in a string variable.
-    pass
+    secret_message_file = pathlib.Path(
+        __file__).parent.resolve() / input_file_path
 
-    # TODO 4. Use your caesar_encrypt(original_text, step) function
+    with open(secret_message_file, 'r') as secret_message:
+        decrypt_message_txt = caesar_encrypt(secret_message.read(), 5)
+
+    #  4. Use your caesar_encrypt(original_text, step) function
     #  to decrypt your friends message and write the result in a new file,
     #  „decrypted_message.txt", using the file handling from previous lessons.
     #  Tipp: your friends favorite number is 5!
-    pass
+    decrypt_message_file = pathlib.Path(
+        __file__).parent.resolve() / output_file_path
+
+    with open(decrypt_message_file, 'w') as decrypt_message:
+        decrypt_message.write(decrypt_message_txt)
+
+
+decypher_file('secret_message.txt', 'output.txt')
