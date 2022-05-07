5c5558207bd782b7a5e7ab4b5dc9ef36fff6f236
FayJu <faizan@unetiq.com>
2022-05-07 14:43:30 +0200

    c14 animal updated

--- assignments/c14_oop_inheritance/animal.py
+++ assignments/c14_oop_inheritance/animal.py
@@ -29,6 +29,35 @@ class Fish(Animal):
         self.number_of_fins = number_of_fins
 
 
+def count_legs(animal_list):
+    leg_count = 0
+    for animal in animal_list:
+        # use instance check (this is better)
+        if isinstance(animal, Mammal) or isinstance(animal, Bird):
+            # if animal.category in ["Mammal", "Bird"]:
+            leg_count += animal.number_of_legs
+    return leg_count
+
+
+def count_category(animal_list):
+    categories = {}
+    for animal in animal_list:
+        if animal.category in categories:
+            categories[animal.category] += 1
+        else:
+            categories[animal.category] = 1
+    return categories
+
+
+animal_list = [
+    Fish('Nemo', 'Fish', 2, 7),
+    Fish("Marti", "Fish", 2, 7),
+    Mammal("Bambi", "Mammal", 1, 4),
+    Mammal("Simba", "Mammal", 3, 4),
+    Bird("Iago", "Bird", 4, 2)
+]
+
+
 if __name__ == "__main__":
     animal = Animal("Lion",   "Mammal", 10)
     animal.print()
@@ -42,34 +71,8 @@ if __name__ == "__main__":
     lago = Bird('Lago', 'Bird', 4, 2)
     lago.print()
 
-    animal_list = [
-        Fish('Nemo', 'Fish', 2, 7),
-        Fish("Marti", "Fish", 2, 7),
-        Mammal("Bambi", "Mammal", 1, 4),
-        Mammal("Simba", "Mammal", 3, 4),
-        Bird("Iago", "Bird", 4, 2)
-    ]
-
-    def count_legs(animal_list):
-        leg_count = 0
-        for animal in animal_list:
-            # use instance check (this is better)
-            if isinstance(animal, Mammal) or isinstance(animal, Bird):
-                # if animal.category in ["Mammal", "Bird"]:
-                leg_count += animal.number_of_legs
-        return leg_count
-
     legs = count_legs(animal_list)
     print(f'legs = {legs}')
-
-    def count_category(animal_list):
-        categories = {}
-        for animal in animal_list:
-            if animal.category in categories:
-                categories[animal.category] += 1
-            else:
-                categories[animal.category] = 1
-        return categories
 
     category = count_category(animal_list)
     print(category)
